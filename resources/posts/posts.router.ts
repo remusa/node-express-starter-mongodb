af8c6fa29c63f6d3c1bbc9b86db2cb32391792da
R Sánchez <rene.as.himself@gmail.com>
2020-08-06 19:51:15 -0500

    Added Post resource

---
+++ resources/posts/posts.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import middleware from '../../utils/middleware'
+import controller from './posts.controllers'
+import { ensureOwnerOrAdmin } from '../../utils/auth'
+import { Post } from './posts.model'
+
+const router: Router = Router()
+
+router.route('/').get(controller.getPosts()).post(controller.createPost())
+
+router.use('/:id', [middleware.validateObjectId, middleware.setModel(Post), ensureOwnerOrAdmin])
+router.route('/:id').get(controller.getOne).put(controller.updateOne).delete(controller.deleteOne)
+
+export default router
