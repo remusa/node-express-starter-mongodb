23f0deaaabe6563586679cbe903ce1d37782a428
R Sánchez <rene.as.himself@gmail.com>
2020-08-06 19:48:26 -0500

    feat: validateObjectId middleware

--- resources/users/users.router.ts
+++ resources/users/users.router.ts
@@ -7,7 +7,10 @@ const router: Router = Router()
 router.route('/me').get(controller.getMe()).put(controller.updateMe())
 
 router.use('/', ensureAdmin)
+
 router.route('/').get(controller.getMany).post(controller.createOne)
+
+router.use('/:id', [middleware.validateObjectId])
 router.route('/:id').get(controller.getOne).put(controller.updateOne).delete(controller.deleteOne)
 
 export default router
